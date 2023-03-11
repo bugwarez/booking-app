0c6dd9db4a47f7e9719533e2d57ba3d116b48cb9
SirMacr0 <apply.tuna@gmail.com>
2023-03-11 13:28:13 +0300

    init project

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+    //
+    var conferenceName = "Gophers"
+    const conferenceTickets = 50
+    fmt.Println("Welcome to", conferenceName, "Booking app")
+    fmt.Println("Get your tickets!")
+
+}
